4d04870a66ab96ccdf56c0fcac7a3270f73661cf
balin-sv <balinsvet@gmail.com>
2024-11-14 05:53:57 -0800

    add .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,10 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "prettier/prettier": "off",
   },
 };
